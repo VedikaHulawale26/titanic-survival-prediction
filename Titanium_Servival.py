c2b9edd4a5c470b58dde9e62d9311074d33e427a
VedikaHulawale26 <vedikahulawale33@gmail.com>
2025-09-27 13:40:08 +0530

    Import librarys

---
+++ Titanium_Servival.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import  root_mean_squared_error
+
